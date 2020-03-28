53196895012136290951aff0b52c0f7d4d39c57f
Rakesh Swain <swain.rakesh131@gmail.com>
2020-03-28 17:17:17 +0530

    new

--- coronadashboard/settings.py
+++ coronadashboard/settings.py
@@ -121,4 +121,4 @@ STATIC_URL = '/static/'
 
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
