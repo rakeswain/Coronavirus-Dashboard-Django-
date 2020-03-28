f3d7e03d6b21d9df8310ea598043db48f854468b
Rakesh Swain <swain.rakesh131@gmail.com>
2020-03-28 17:15:09 +0530

    new

--- coronadashboard/settings.py
+++ coronadashboard/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -118,3 +119,6 @@ STATICFILES_DIRS = (
 )
 STATIC_URL = '/static/'
 
+
+#  Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
